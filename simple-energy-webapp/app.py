f7c13c8d64b6a0ed2d8af6316c96986f982ed20e
Shodhan <shodhan@simpleenergy.in>
2025-11-20 19:18:59 +0530

    new updated file

--- simple-energy-webapp/app.py
+++ simple-energy-webapp/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """
-    <h1>Welcome to Simple Energy 🚴‍♂️⚡</h1>
-    <img src="https://www.simpleenergy.in/_next/image?url=%2Fimages%2Fhome%2Fbanner.webp&w=1920&q=75" alt="Simple Energy EV Bike" width="600">
+    return f"""
+    <h1>welcome to EVR 🚴‍♂️⚡</h1>
+    <img src="{url_for('static', filename='segway-xyber-kv-making.webp')}" alt="Simple Energy EV Bike" width="600">
     """
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
